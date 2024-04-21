44a9ec88aa27215693b6f30f38f0dac034360146
YahYahDev <thomasbrianburkhardt@gmail.com>
2024-04-21 15:35:26 -0400

    updated plug to unpack plugins

--- src/Server/Plug.lua
+++ src/Server/Plug.lua
@@ -10,11 +10,31 @@ local log = require("Modules.Std.Log")
 
 ---@class Plug
 local plug = {
-
+---
+---@param self Plug
+---@param PlugPath string
+---@return nil Failed
 	Unpack = function (self, PlugPath)
 
+		local file = io.open(PlugPath,"r")
+		if file == nil then
+			log:Error("Failed to Load Plugin from: "..PlugPath)
+			file:close()
+			return nil
+		end
+		local content = file:read("a")
+		file:close()
+		-- Parse all blocks of code in a plugin
+		local chunks = parse.GetAllBlock(content, "start:", "end:")
 
-
+		-- Sets func names to key in plug
+		log:Add("Loading Plugin: "..PlugPath)
+		for i = 1, #chunks do
+			local key = parse.GetBlock(chunks[1], "", "\n")
+			local func = parse.GetBlock(chunks[1], "\n", "\n$")
+			self[key] = load(func)
+			log:Add("   Loaded: "..key)
+		end
 	end,
 ---@param self Plug
 ---@param PlugDir string
@@ -24,15 +44,13 @@ local plug = {
 		if pluglist == nil then
 			log:Error("Failed to Load Plugins from: "..PlugDir)
 		else
-			log:Add("Loaded Plugins from: "..PlugDir)
+			log:Add("Loading Plugins from: "..PlugDir)
 		end
 		pluglist = parse.GetAllBlock(pluglist, "", "\n")
 
 		-- Plug:Unpack() all the plugin files
 		for i = 1, #pluglist do
-
-
-
+			self:Unpack(PlugDir.."/"..pluglist[i])
 		end
 
 	end,
